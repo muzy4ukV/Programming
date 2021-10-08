465659c6d602ce9836912f4a8561ed50c5d83cb9
muzychukV <muzychuk.vitaliy@lll.kpi.ua>
2021-10-08 18:58:46 +0300

    Third lab done

--- Python/Lab_3/Lab_3.py
+++ Python/Lab_3/Lab_3.py
@@ -1,17 +1,12 @@
 accuracy = float(input("Введіть точність обчисленння: "))
-e = 1
-i = 1
+e = 0
+i = 0
 fact = 1
-previous = 0
-current = 1 / (fact)
-difference = abs(current - previous)
-
-while difference > accuracy:
-    e = e + current
-    i += 1
-    fact = fact * i
-    previous = current
-    current = 1 / fact
-    difference = abs(current - previous)
+current = 1 / fact  
+while current > accuracy:
+        e = e + current
+        i = i + 1
+        fact = fact * i
+        current = 1 / fact
     
-print(e)
+print("e =", e)
---
+++ С#/Lab_3/Lab_3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab_3
+{
+    class Lab_3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter accuracy: ");
+            double accuracy = Convert.ToDouble(Console.ReadLine());
+            double e = 1;
+            int i = 0;
+            int fact = 1;
+            double current = 0;
+            do
+            {
+                e += current;
+                i += 1;
+                fact *= i;
+                current = 1.0 / fact;
+            } while (current > accuracy);
+
+            Console.WriteLine($"e = {e}");
+        }
+    }
+}
